c02b3e9fd7e1b8b09b062edb84e3e90330b3a93a
fwd370 <fwd1@berkeley.edu>
2022-03-30 18:38:13 +0800

    add css webpack

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,27 +1,30 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-mode: 'development',
-			entry: { app: ['./src/App.jsx'] },
-			output: {
-filename: '[name].bundle.js',
-					path: path.resolve(__dirname, 'public'),
-			},
-module: {
-rules: [
-			 {
-test: /\.jsx?$/,
-			 exclude: /node_modules/,
-			 use: 'babel-loader',
-			 },
-],
-				},
-optimization: {
-splitChunks: {
-name: 'vendor',
-			chunks: 'all',
-						 },
-							},
-devtool: 'source-map',
+  mode: "development",
+  entry: { app: ["./src/App.jsx"] },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "all",
+    },
+  },
+  devtool: "source-map",
 };
-
