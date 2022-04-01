ff76997fe18a8fd36a5d7471fd1937fbc02cda6f
fwd370 <fwd1@berkeley.edu>
2022-04-01 13:05:56 +0800

    restore Contents.jsx

---
+++ ui/src/components/Contents.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import Diet from "./Diet.jsx";
+import Motivation from "./Motivation.jsx";
+import Tracker from "./Tracker.jsx";
+import Train from "./Train.jsx";
+import Home from "./Home.jsx";
+
+const NotFound = () => <h1>Page Not Found</h1>;
+
+export default function Contents() {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/home" />
+      <Route path="/home" component={Home} />
+      <Route path="/diet" component={Diet} />
+      <Route path="/motivation" component={Motivation} />
+      <Route path="/tracker" component={Tracker} />
+      <Route path="/train" component={Train} />
+      <Route path="/setting" component={Setting} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
